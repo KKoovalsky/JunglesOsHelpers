a98d5e9a3a5a0277ad09a3b436fc669e632ecb6d
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2020-05-03 20:36:48 +0200

    FreeRTOS thread test: unique ID for each thread
    For better traceability I have added a unique ID for each thread created
    for testing.

--- test/freertos/test_thread_impl.cpp
+++ test/freertos/test_thread_impl.cpp
@@ -7,9 +7,15 @@
 
 #include "freertos/thread_impl.hpp"
 #include <memory>
+#include <vector>
+#include <string>
 
 
 std::unique_ptr<jungles::thread> get_thread_for_test_run(std::function<void(void)> f)
 {
-    return std::make_unique<jungles::thread_impl<512>>(f, "Test thread", 1);
+    static std::vector<std::string> thread_names;
+    static unsigned thread_num{0};
+    thread_names.push_back("Test_thread_" + std::to_string(thread_num));
+    ++thread_num;
+    return std::make_unique<jungles::thread_impl<512>>(f, thread_names.back(), 1);
 }
