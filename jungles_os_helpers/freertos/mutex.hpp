bc607181432f31e54746f008a481b3142cfe34c9
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-01-23 15:16:27 +0100

    Implement FreeRTOS mutex wrapper

---
+++ jungles_os_helpers/freertos/mutex.hpp
@@ -0,0 +1,48 @@
+/**
+ * @file        mutex.hpp
+ * @brief       Implements a mutex that works with FreeRTOS
+ * @author      Kacper Kowalski - kacper.s.kowalski@gmail.com
+ */
+#ifndef MUTEX_HPP
+#define MUTEX_HPP
+
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+namespace jungles
+{
+
+namespace freertos
+{
+
+class mutex
+{
+  public:
+    explicit mutex() : mux{xSemaphoreCreateMutex()}
+    {
+    }
+
+    void lock()
+    {
+        xSemaphoreTake(mux, portMAX_DELAY);
+    }
+
+    void unlock()
+    {
+        xSemaphoreGive(mux);
+    }
+
+    ~mutex()
+    {
+        vSemaphoreDelete(mux);
+    }
+
+  private:
+    SemaphoreHandle_t mux;
+};
+
+} // namespace freertos
+
+} // namespace jungles
+
+#endif /* MUTEX_HPP */
