349e2185b82dbfdc352efe02b733d2b31949ff23
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-12-13 14:13:29 +0100

    Implement queue which sends from ISR
    This mutates jungles::freertos::queue by sending elements from ISR
    context.

---
+++ jungles_os_helpers/freertos/queue_sending_from_isr.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file	queue_sending_from_isr.hpp
+ * @brief	Implements queue which sends from ISR context only (won't work from task).
+ * @author	Kacper Kowalski - kacper.s.kowalski@gmail.com
+ */
+#ifndef FREERTOS_QUEUE_SENDING_FROM_ISR_HPP
+#define FREERTOS_QUEUE_SENDING_FROM_ISR_HPP
+
+#include "queue.hpp"
+
+namespace jungles
+{
+
+namespace freertos
+{
+
+/**
+ * @brief Sends messages from ISR context.
+ *
+ * This changes jungles::freertos::queue behavior in such a way, that when send() is called it is performed with
+ * FreeRTOS API which can only be used from ISR context. The primary queue implementation can only be used from task
+ * context.
+ *
+ */
+template<typename ElementType, std::size_t Size>
+class queue_sending_from_isr : jungles::freertos::queue<ElementType, Size>
+{
+  public:
+    void send(ElementType&& elem)
+    {
+        insert(std::move(elem));
+
+        auto r{xSemaphoreGiveFromISR(num_elements_counting_sem)};
+        assert(r == pdTRUE);
+    }
+};
+
+} // namespace freertos
+
+} // namespace jungles
+
+#endif /* FREERTOS_QUEUE_SENDING_FROM_ISR_HPP */
