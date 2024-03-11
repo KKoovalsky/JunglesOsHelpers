44609cac64670414213084c3f776c5b5d3fb8f51
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2024-03-11 13:44:23 +0100

    Implement enum to bits converter
    The main idea is to convert, e.g.:
    
        to_bits(1, 4, 5);
    
    To:
        (1 << 1) | (1 << 4) | (1 << 5)
    
    and use common underlying enumeration type, to force the users to use:
    
        to_bits(SomeEnum::field1, SomeEnum::field3, ...)
    
    And perform compile time checks, that all the enumerations come from the
    same `enum` or `enum class`.

---
+++ jungles_os_helpers/utils/enum_to_bits.hpp
@@ -0,0 +1,112 @@
+/**
+ * @file        enum_to_bits.hpp
+ * @brief       Converts enums to bits, aka: enum class { one = 0, two = 1, ... } -> { 0b01, 0b10, ... } (powers of 2).
+ */
+#ifndef ENUM_TO_BITS_HPP
+#define ENUM_TO_BITS_HPP
+
+#include <algorithm>
+#include <array>
+#include <tuple>
+#include <type_traits>
+
+namespace jungles::utils
+{
+
+namespace detail
+{
+template<typename T, typename U>
+struct is_same_type : std::false_type
+{
+};
+
+template<typename T>
+struct is_same_type<T, T> : std::true_type
+{
+};
+
+template<typename... Types>
+struct are_all_same;
+
+template<typename T, typename... Rest>
+struct are_all_same<T, Rest...> : std::conjunction<is_same_type<T, Rest>...>
+{
+};
+
+template<auto... Vs>
+struct unique_values
+{
+  private:
+    static inline constexpr unsigned get_num_unique_values()
+    {
+        std::array vs{Vs...};
+        auto new_end{std::unique(std::begin(vs), std::end(vs))};
+        return std::distance(std::begin(vs), new_end);
+    }
+
+  public:
+    static inline constexpr auto value{get_num_unique_values()};
+};
+
+template<typename... Vs>
+struct first_type
+{
+    using value = typename std::tuple_element_t<0, std::tuple<Vs...>>;
+};
+
+}; // namespace detail
+
+template<typename UnderlyingType = unsigned, auto... Events>
+struct EnumToBits
+{
+  private:
+    static inline constexpr auto MinEnumValue{std::min({Events...})};
+    static inline constexpr auto MaxEnumValue{std::max({Events...})};
+
+  public:
+    static_assert(std::is_trivial_v<UnderlyingType>);
+    static_assert(std::is_enum_v<decltype(Events)...>, "Use 'enum' or 'enum class' to define the events");
+    static_assert(detail::are_all_same<decltype(Events)...>::value, "The enumerations must have the same type");
+    static_assert(detail::unique_values<Events...>::value <= sizeof(UnderlyingType),
+                  "Maximum of sizeof(UnderlyingType) events are supported");
+    static_assert(MaxEnumValue <= sizeof(UnderlyingType), "The maximum enumeration value is too high");
+    static_assert(MinEnumValue >= 0, "Only non-negative values are supported");
+
+  private:
+    static inline constexpr auto make_bit_values()
+    {
+        std::array evts{Events...};
+        std::array<UnderlyingType, MaxEnumValue + 1> bit_values = {};
+        for (auto e : evts)
+        {
+            auto underlying_value{static_cast<unsigned>(e)};
+            bit_values[underlying_value] = 1 << underlying_value;
+        }
+        return bit_values;
+    }
+
+    static inline constexpr std::array bit_values{make_bit_values()};
+
+  public:
+    template<typename... Ts>
+    static inline constexpr UnderlyingType to_bits(Ts... events)
+    {
+        static_assert(detail::are_all_same<Ts...>::value);
+        static_assert(std::is_same_v<typename detail::first_type<Ts...>::value,
+                                     typename detail::first_type<decltype(Events)...>::value>);
+
+        std::array evts{events...};
+        UnderlyingType bits{0};
+        for (auto e : evts)
+        {
+            auto underlying_value{static_cast<unsigned>(e)};
+            bits |= 1 << underlying_value;
+        }
+
+        return bits;
+    }
+};
+
+}; // namespace jungles::utils
+
+#endif /* ENUM_TO_BITS_HPP */
