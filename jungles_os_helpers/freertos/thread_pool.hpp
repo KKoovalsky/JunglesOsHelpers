fa590f4bbad87dae6f970406dc1985ddd9e9458b
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2024-03-19 00:37:56 +0100

    Test thread pool with freertos

---
+++ jungles_os_helpers/freertos/thread_pool.hpp
@@ -0,0 +1,57 @@
+/**
+ * @file        thread_pool.hpp
+ * @brief       The thread pool for the FreeRTOS target.
+ */
+#ifndef FREERTOS_THREAD_POOL_HPP
+#define FREERTOS_THREAD_POOL_HPP
+
+#include <mutex>
+
+#include "jungles_os_helpers/generic/thread_pool.hpp"
+
+#include "jungles_os_helpers/freertos/mutex.hpp"
+#include "jungles_os_helpers/freertos/queue.hpp"
+#include "jungles_os_helpers/freertos/thread.hpp"
+
+namespace freertos
+{
+
+struct ThreadPoolConfig
+{
+    constexpr ThreadPoolConfig(std::size_t runner_stack_size, unsigned runner_priority) :
+        runner_stack_size{runner_stack_size}, runner_priority{runner_priority}
+    {
+    }
+
+    const std::size_t runner_stack_size;
+    const std::size_t runner_priority;
+};
+
+namespace detail
+{
+
+template<os::generic::RunnersCount RC, std::size_t QueueSize, ThreadPoolConfig Config>
+struct thread_pool_impl
+{
+    template<typename ElementType>
+    using Queue = jungles::freertos::queue<ElementType, QueueSize>;
+
+    using Mutex = jungles::freertos::mutex;
+
+    struct Thread : jungles::freertos::thread
+    {
+        Thread() : jungles::freertos::thread{"ThreadFromPool", Config.runner_stack_size, Config.runner_priority}
+        {
+        }
+    };
+    using type = os::generic::thread_pool<RC, Thread, Queue, Mutex>;
+};
+
+}; // namespace detail
+
+template<os::generic::RunnersCount RC, std::size_t QueueSize, ThreadPoolConfig TPC>
+using thread_pool = typename detail::thread_pool_impl<RC, QueueSize, TPC>::type;
+
+} // namespace freertos
+
+#endif /* FREERTOS_THREAD_POOL_HPP */
---
+++ test/freertos/thread_pool_under_test.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file	thread_pool_under_test.hpp
+ * @brief	Defines the Thread Pool to be tested for FreeRTOS target.
+ */
+#ifndef THREAD_POOL_UNDER_TEST_DEFINITION_HPP
+#define THREAD_POOL_UNDER_TEST_DEFINITION_HPP
+
+#include "jungles_os_helpers/freertos/thread_pool.hpp"
+
+namespace test
+{
+
+template<os::generic::RunnersCount RC>
+auto make_thread_pool()
+{
+    constexpr unsigned queue_size{16};
+    constexpr freertos::ThreadPoolConfig config{2048, 1};
+    return freertos::thread_pool<RC, queue_size, config>();
+}
+
+} // namespace test
+
+#endif /* THREAD_POOL_UNDER_TEST_DEFINITION_HPP */
